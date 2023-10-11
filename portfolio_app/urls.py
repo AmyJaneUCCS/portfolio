d3a139760347692607bcf79c38d8f6f34e8d85c6
AmyJane <amyjane.reilly@gmail.com>
2023-10-11 10:00:38 -0600

    areilly: GE04 Creating portfolio list and detail views

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,4 +1,4 @@
-from .views import StudentListView, StudentDetailView
+from .views import StudentListView, StudentDetailView, PortfolioListView, PortfolioDetailView
 from django.urls import path
 from . import views
 
@@ -7,5 +7,7 @@ urlpatterns = [
     # '' is empty to represent based path to app
     path('', views.index, name='index'),
     path('students/', StudentListView.as_view(), name= 'students'),
-    path('student/<int:pk>', StudentDetailView.as_view(), name='student-detail')
+    path('student/<int:pk>', StudentDetailView.as_view(), name='student-detail'),
+    path('portfolios/', PortfolioListView.as_view(), name= 'portfolios'),
+    path('portfolio/<int:pk>', PortfolioDetailView.as_view(), name='portfolio-detail'),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Student
+from .models import Student, Portfolio
 
 class StudentListView(generic.ListView):
     model = Student
 class StudentDetailView(generic.DetailView):
     model = Student
 
+class PortfolioListView(generic.ListView):
+    model = Portfolio
+class PortfolioDetailView(generic.DetailView):
+    model = Portfolio
+
 # Create your views here.
 def index(request):
-
-    # Render the HTTP template index.html with the data in the context variable
-    return render(request, 'portfolio_app/index.html')
+    student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio__is_active=True)
+    print("active portfolio query set", student_active_portfolios)
+    return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
