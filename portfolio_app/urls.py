a057701409ea6a3c0436466762a98591a61aece7
AmyJane <amyjane.reilly@gmail.com>
2023-10-11 08:57:05 -0600

    areilly: GE03 and part of GE05

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,3 +1,4 @@
+from .views import StudentListView, StudentDetailView
 from django.urls import path
 from . import views
 
@@ -5,4 +6,6 @@ urlpatterns = [
     # path function defines a url pattern
     # '' is empty to represent based path to app
     path('', views.index, name='index'),
+    path('students/', StudentListView.as_view(), name= 'students'),
+    path('student/<int:pk>', StudentDetailView.as_view(), name='student-detail')
 ]
